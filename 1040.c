076aad7e05f4dbbb96ea1533039f39c2271a1e3e
Diego Dall'Agnol <diegodagnol@gmail.com>
2023-08-14 13:47:38 -0300

    Create 1040.c

---
+++ 1040.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main()
+{
+
+    double n1, n2, n3, n4, media, exame;
+
+    scanf("%lf %lf %lf %lf", &n1, &n2, &n3, &n4);
+
+    media = (n1 * 2 + n2 * 3 + n3 * 4 + n4 * 1) / 10;
+    printf("Media: %.1lf\n", media);
+
+    if (media >= 7.0)
+    {
+        printf("Aluno aprovado.");
+    }
+    else if (media < 5.0)
+    {
+        printf("Aluno reprovado.\n");
+    }
+    else if (media >= 5.0 || media <= 6.9)
+    {
+        printf("Aluno em exame.\n");
+        scanf("%lf", &exame);
+        printf("Nota do exame: %.1lf\n", exame);
+        media = (media + exame) / 2;
+
+        if (media >= 5.0)
+        {
+            printf("Aluno aprovado.\n");
+            printf("Meida final: %.1lf\n", media);
+        }
+        else
+        {
+            printf("Aluno reprovado.\n");
+            printf("Meida final: %.1lf\n", media);
+        }
+    }
+
+    return 0;
+}
