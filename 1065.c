f6dc31aab57b05c42aea25f8fcd8b4ca54f89df5
diegodagnol <93943178+diegodagnol@users.noreply.github.com>
2023-08-14 11:33:33 -0300

    Create 1065.c

---
+++ 1065.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int i, n, cont;
+    cont = 0;
+    for(i = 1; i <= 5; i++){
+        scanf("%d", &n);
+        if (n%2==0){
+            cont++;
+        }
+    }
+    printf("%d valores pares\n", cont);
+ 
+    return 0;
+}
